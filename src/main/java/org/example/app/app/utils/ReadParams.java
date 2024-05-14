702a557adccad872036a418dc349ef2ae853b6eb
Zhoholiev Pavlo <zhogolevpv@gmail.com>
2024-05-14 10:43:25 +0300

    remove not use methods 14.05.2024 10:42 Zhoholiev Pavlo

--- src/main/java/org/example/app/app/utils/ReadParams.java
+++ src/main/java/org/example/app/app/utils/ReadParams.java
@@ -1,6 +1,4 @@
 package org.example.app.app.utils;
-
-import org.example.app.app.entity.User;
 import org.example.app.app.utils.validate.Validator;
 import org.example.app.app.utils.validate.enums.EValidateQuery;
 import org.example.app.app.utils.validate.validate_entity.ValidateAnswer;
@@ -9,52 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ReadParams {
-    private int limit;
-    private int offset;
-    private List<String> excludeColumns;
+    private final int limit;
+    private final int offset;
+    private final List<String> excludeColumns;
     private final Validator<EValidateQuery> validator = new Validator<>();
     private Long id;
 
-    public ValidateAnswer setLimit(String limit) {
-        ValidateAnswer answer = validator.validate(limit, EValidateQuery.LIMIT);
-        if (answer.isValid()) {
-            this.limit = Integer.parseInt(limit);
-            return answer;
-        }
-        return answer;
-    }
-
-    public ValidateAnswer setOffset(String offset) {
-        ValidateAnswer answer = validator.validate(offset, EValidateQuery.OFFSET);
-        if (answer.isValid()) {
-            this.offset = Integer.parseInt(offset);
-            return answer;
-        }
-        return answer;
-    }
-
-    public ValidateAnswer setExcludeColumns(String excludeColumns) {
-        ValidateAnswer answer = validator.validate(excludeColumns, EValidateQuery.EXCLUDE_COLUMNS);
-        List<String> columnsNotInTable = new ArrayList<>();
-        if (answer.isValid()) {
-            List<String> columnsFromUser= List.of(excludeColumns.split(","));
-            AnnotationUtils annotationUtils = new AnnotationUtils();
-            List<String> columnsInTable = annotationUtils.getColumnNames(User.class);
-            columnsFromUser.forEach(column -> {
-                if (!columnsInTable.contains(column)) {
-                    columnsNotInTable.add(column);
-                }
-            });
-            if (!columnsNotInTable.isEmpty()) {
-                ValidateAnswer errorAnswer = new ValidateAnswer();
-                columnsNotInTable.forEach(column -> errorAnswer.addError("Column " + column + " not in table"));
-                return errorAnswer;
-            }
-            this.excludeColumns = columnsFromUser;
-            return answer;
-        }
-        return answer;
-    }
 
     public ValidateAnswer setId(String id) {
         ValidateAnswer answer = validator.validate(id, EValidateQuery.ID);
